ee2a885cebf9a68b1bc6df0e27d302dcab2fb51a
kartiksapkal <kartiksapkal5799@gmail.com>
2024-11-26 10:30:40 +0530

    add error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -16,32 +17,45 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     }
 
     // Check if user already exists
-    const user = await userModel.findOne({ email: email });
+    let user;
+    try {
+      user = await userModel.findOne({ email: email });
+    } catch (error) {
+      return next(createHttpError(500, "Error while fetching user"));
+    }
+
     if (user) {
       const error = createHttpError(400, "User already exists");
       return next(error);
     }
 
     // Process
-    const hashPassword = await bcrypt.hash(password, 10);
+    let newUser: User;
+    try {
+      const hashPassword = await bcrypt.hash(password, 10);
 
-    // Create user and await result
-    const newUser = await userModel.create({
-      name,
-      email,
-      password: hashPassword,
-    });
-    //jwt token
-    const token = sign(
-      {
-        sub: newUser._id,
-      },
-      config.jwtSecret as string,
-      { expiresIn: "7d" ,algorithm:'ES256',}
-    );
+      newUser = await userModel.create({
+        name,
+        email,
+        password: hashPassword,
+      });
+    } catch (error) {
+      return next(createHttpError(500, "Error while creating user"));
+    }
 
-    // Response
-    res.status(201).json({ accessToken: token });
+    // JWT token
+    try {
+      const token = sign(
+        { sub: newUser._id },
+        config.jwtSecret as string, 
+        { expiresIn: "7d", algorithm: "HS256" }
+      );
+
+      // Response
+      res.status(201).json({ accessToken: token });
+    } catch (error) {
+      return next(createHttpError(500, "Error while signing JWT"));
+    }
   } catch (err) {
     next(err);
   }
